665b8292e178fd1c4ba23f0ea149e2e6a5349289
Siddharth Prasad <siddharth.prasad@happiestminds.com>
2018-02-19 12:09:52 +0530

    backup

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.index, name='index'),
-               path('<int:pk>/', views.details, name='id'),
-               path('<int:pk>/results/',
+               path('<int:user_id>/', views.details, name='id'),
+               path('<int:user_id>/results/',
                     views.results, name='results'),
                path('<int:user_id>/name/', views.names, name='vote'),
                path('specifics/<int:user_id>/', views.details, name='details'), ]
